939563fc4ee8bd0d2e742f77170ec59b35093143
Spirit <Leonidmaslennikov39890@gmail.com>
2026-04-10 18:48:21 +0300

    HW for 11.04.2026

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
+import java.util.Properties
+
 plugins {
     id("java")
     id("org.springframework.boot") version "3.4.4"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.liquibase.gradle") version "2.2.2"
 }
 
 group = "kfu.itis.maslennikov"
@@ -23,7 +26,30 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("javax.mail:javax.mail-api:1.6.2")
     implementation("org.springframework.security:spring-security-taglibs:${springSecurityVersion}")
+    // стартер для ликвидбейза ->
+    implementation("org.liquibase:liquibase-core:4.33.0")
+    liquibaseRuntime("org.liquibase:liquibase-core:4.33.0")
+    liquibaseRuntime("org.postgresql:postgresql:$postgresVersion")
+    // библиотека для работы с аргументами командной строки пикокли ->
+    liquibaseRuntime("info.picocli:picocli:4.6.3")
 }
+
+val props = Properties()
+props.load(file("src/main/resources/db/liquibase.properties").inputStream())
+
+
+liquibase {
+    activities.register("main") {
+        arguments = mapOf(
+            "changeLogFile" to props.get("change-log-file"),
+            "url" to props.get("url"),
+            "username" to props.get("username"),
+            "password" to props.get("password"),
+            "driver" to props.get("driver-class-name")
+        )
+    }
+}
+
 
 tasks.test {
     useJUnitPlatform()
